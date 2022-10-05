b5ca1dea9d078d25d533bdf5fa4a924883467884
Fufette <galaffu.dev@gmail.com>
2022-10-05 04:40:31 +0200

    Mise en place du projet
    Symfony translate | Login/Register | Bootstrap 5

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+
+    /**
+ * @Route("/change_locale/{locale}", name="change_locale")
+ */
+public function changeLocale($locale, Request $request)
+{
+    // On stocke la langue dans la session
+    $request->getSession()->set('_locale', $locale);
+
+    // On revient sur la page précédente
+    return $this->redirect($request->headers->get('referer'));
+}
+
+
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
